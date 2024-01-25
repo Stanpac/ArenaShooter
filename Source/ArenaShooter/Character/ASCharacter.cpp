dbc641f87c156ef2e8a979a095796900cb009c9e
Stanislas <fritschstanislas@gmail.com>
2024-01-25 23:16:21 +0100

    Input and basic

--- Source/ArenaShooter/Character/ASCharacter.cpp
+++ Source/ArenaShooter/Character/ASCharacter.cpp
@@ -2,22 +2,117 @@
 
 
 #include "ArenaShooter/Character/ASCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 
 AASCharacter::AASCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
 	PrimaryActorTick.bStartWithTickEnabled = false;
+	
+	UCapsuleComponent* CapsuleComp = GetCapsuleComponent();
+	check(CapsuleComp);
+	CapsuleComp->InitCapsuleSize(40.0f, 90.0f);
+
+	USkeletalMeshComponent* MeshComp = GetMesh();
+	check(MeshComp);
+	MeshComp->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));  // Rotate mesh to be X forward since it is exported as Y forward.
+
+	// Character Movement (Lyra Params)
+	UCharacterMovementComponent* MoveComp = CastChecked<UCharacterMovementComponent>(GetCharacterMovement());
+	MoveComp->GravityScale = 1.0f;
+	MoveComp->MaxAcceleration = 2400.0f;
+	MoveComp->BrakingFrictionFactor = 1.0f;
+	MoveComp->BrakingFriction = 6.0f;
+	MoveComp->GroundFriction = 8.0f;
+	MoveComp->BrakingDecelerationWalking = 1400.0f;
+	MoveComp->bUseControllerDesiredRotation = false;
+	MoveComp->bOrientRotationToMovement = false;
+	MoveComp->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+	MoveComp->bAllowPhysicsRotationDuringAnimRootMotion = false;
+	MoveComp->GetNavAgentPropertiesRef().bCanCrouch = true;
+	MoveComp->bCanWalkOffLedgesWhenCrouching = true;
+	MoveComp->SetCrouchedHalfHeight(65.0f);
+
+	m_FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	m_FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	m_FirstPersonCameraComponent->SetRelativeLocation(FVector(-10.f, 0.f, 60.f)); // Position the camera
+	m_FirstPersonCameraComponent->bUsePawnControlRotation = true;
+	
+	MeshComp->SetupAttachment(m_FirstPersonCameraComponent);
 }
 
 void AASCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	AddDefaultMappingContext();
 }
 
 void AASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
+		
+		// Jumping
+		EnhancedInputComponent->BindAction(m_JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(m_JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
+		// Moving
+		EnhancedInputComponent->BindAction(m_MoveAction, ETriggerEvent::Triggered, this, &AASCharacter::Move);
+
+		// Looking
+		EnhancedInputComponent->BindAction(m_LookAction, ETriggerEvent::Triggered, this, &AASCharacter::Look);
+		
+	} else {
+		UE_LOG(LogTemp, Error, TEXT("'%s' Don't Find EnhancedInputComponent."), *GetNameSafe(this));
+	}
 }
+
+void AASCharacter::Move(const FInputActionValue& Value)
+{
+	
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr) {
+		
+		// add movement 
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
+}
+
+void AASCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr) {
+		
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void AASCharacter::AddDefaultMappingContext()
+{
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))  {
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) {
+			Subsystem->AddMappingContext(m_DefaultMappingContext, 0);
+		}
+	}
+}
+
+void AASCharacter::RemoveDefaultMappingContext()
+{
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))  {
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) {
+			Subsystem->RemoveMappingContext(m_DefaultMappingContext);
+		}
+	}
+}
+
+
 
--- Source/ArenaShooter/Character/ASCharacter.h
+++ Source/ArenaShooter/Character/ASCharacter.h
@@ -6,20 +6,63 @@
 #include "GameFramework/Character.h"
 #include "ASCharacter.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+class UCameraComponent;
+class USkeletalMeshComponent;
+struct FInputActionValue;
+
+/**
+ * Base Class for all Characters in the game (Player and AI)
+ * TODO :  Bind Input Actions to Functions
+ * TODO :  Implement jump, Sprint ? Crouch ?
+ * TODO :  Implement Death
+ * TODO :  Implement Health -> Component 
+ * 
+ */
+
 UCLASS()
 class ARENASHOOTER_API AASCharacter : public ACharacter
 {
 	GENERATED_BODY()
 	/* ---------------------------------- MEMBERS --------------------------------------*/
 protected:
+	/** Skeletal Mesh */
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	USkeletalMeshComponent* m_SkeletalMeshComponent;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ASCharacter|Camera", meta = (DisplayName = "FirstPersonCameraComponent"))
+	UCameraComponent* m_FirstPersonCameraComponent;
+
+	/** Default MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ASCharacter|Input", meta=(DisplayName = "DefaultMappingContext"))
+	UInputMappingContext* m_DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ASCharacter|Input", meta=(DisplayName = "JumpAction"))
+	UInputAction* m_JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ASCharacter|Input", meta=(DisplayName = "MoveAction"))
+	UInputAction* m_MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ASCharacter|Input", meta = (DisplayName = "LookAction"))
+	UInputAction* m_LookAction;
 	
 	/* ---------------------------------- FUNCTIONS --------------------------------------*/
-protected:
-	
 public:
 	AASCharacter();
 	
 	virtual void BeginPlay() override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+	void AddDefaultMappingContext();
+
+	void RemoveDefaultMappingContext();
 };
