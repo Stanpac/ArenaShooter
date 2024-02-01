15248848e397286a9cb560e83d494f4d6840c8c1
Stanpac <fritschstanislas@gmail.com>
2024-02-01 17:38:09 +0100

    Turret Life Show, WIP Affichage Life And Turret Shoot

--- Source/ArenaShooter/Weapons/ASWeapon_PlayerPrimary.cpp
+++ Source/ArenaShooter/Weapons/ASWeapon_PlayerPrimary.cpp
@@ -33,8 +33,8 @@ void AASWeapon_PlayerPrimary::Fire(FVector FireOrigin, FVector FireDirection)
 			
 			UASHealthComponent* HealthComponent = UASHealthComponent::FindHealthComponent(HitActor);
 			if(HealthComponent != nullptr) {
+
 				HealthComponent->Damage(m_DamageByBullet, GetOwner());
-				UE_LOG(LogTemp, Warning, TEXT("Shot Hit"));
 				GEngine->AddOnScreenDebugMessage(1, 2, FColor::Red, TEXT("Target Hit"));
 			}
 		} else {
