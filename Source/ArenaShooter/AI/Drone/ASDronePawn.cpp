b9ccd651d1376dd66030a6f2ec0e4a109e975b80
Sneakysunset <andrelauzier@gmail.com>
2024-03-04 23:54:03 +0100

    Behaviour back in c++ With no BT

--- Source/ArenaShooter/AI/Drone/ASDronePawn.cpp
+++ Source/ArenaShooter/AI/Drone/ASDronePawn.cpp
@@ -1,25 +1,24 @@
 #include "ASDronePawn.h"
 #include "ArenaShooter/Components/ASHealthComponent.h"
 #include "ArenaShooter/Components/ASWeaponComponent.h"
-#include "ArenaShooter/Weapons/ASWeapon.h"
 #include "GameFramework/Character.h"
-// Sets default values
+
 AASDronePawn::AASDronePawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	/*m_HealthComponent = CreateDefaultSubobject<UASHealthComponent>(TEXT("HealthComponent"));
+	m_HealthComponent = CreateDefaultSubobject<UASHealthComponent>(TEXT("HealthComponent"));
 
-	m_WeaponComponent = CreateDefaultSubobject<UASWeaponComponent>(TEXT("WeaponComponent"));*/
+	m_WeaponComponent = CreateDefaultSubobject<UASWeaponComponent>(TEXT("WeaponComponent"));
 }
 
-// Called when the game starts or when spawned
 void AASDronePawn::BeginPlay()
 {
 	Super::BeginPlay();
 	m_Player = GetWorld()->GetFirstPlayerController()->GetCharacter();
 	check(m_Player);
+	m_CurrentBehaviour = EDroneBehaviour::CHASING;
+	OnBehaviourStateEnter();
 }
 
 void AASDronePawn::LookAtPlayer(float DeltaTime)
@@ -30,14 +29,195 @@ void AASDronePawn::LookAtPlayer(float DeltaTime)
 	SetActorRotation(LookAtRotation);
 }
 
-// Called every frame
+void AASDronePawn::ChangeBehaviour(EDroneBehaviour newBehaviour)
+{
+	OnBehaviourStateExit();
+	m_CurrentBehaviour = newBehaviour;
+	OnBehaviourStateExit();
+}
+
+void AASDronePawn::OnBehaviourStateEnter()
+{
+	switch(m_CurrentBehaviour)
+	{
+		case EDroneBehaviour::CHASING:
+			break;
+		case EDroneBehaviour::SHOOTING:
+			break;
+		case EDroneBehaviour::TELEPORTING:
+			m_TeleportationTimer = m_TeleportationDuration;
+			break;
+	}
+}
+
+void AASDronePawn::OnBehaviourStateExit()
+{
+	switch(m_CurrentBehaviour)
+	{
+		case EDroneBehaviour::CHASING:
+			break;
+		case EDroneBehaviour::SHOOTING:
+			break;
+		case EDroneBehaviour::TELEPORTING:
+			break;
+	}
+}
+
+void AASDronePawn::OnBehaviourStateTick(float DeltaTime)
+{
+	switch(m_CurrentBehaviour)
+	{
+		case EDroneBehaviour::CHASING:
+			break;
+		case EDroneBehaviour::SHOOTING:
+			break;
+		case EDroneBehaviour::TELEPORTING:
+			m_TeleportationTimer -= DeltaTime;
+			if(m_TeleportationTimer <= 0)
+			{
+				TeleportDrone();
+			}
+			break;
+	}
+}
+
+void AASDronePawn::OnBehaviourAIStateTick()
+{
+	m_DroneToPlayerMult = 0;
+	switch(m_CurrentBehaviour)
+	{
+		case EDroneBehaviour::CHASING:
+			ChasingBehaviour();
+			break;
+		case EDroneBehaviour::SHOOTING:
+			float distanceToObject;
+			if(!IsPlayerInSight(distanceToObject) || distanceToObject < m_MaxRangeToAttack || distanceToObject > m_MinRangeToAttack)
+			{
+				ChangeBehaviour(EDroneBehaviour::CHASING);
+			}
+			else m_WeaponComponent->Fire(GetActorLocation(), GetActorForwardVector());
+			break;
+		case EDroneBehaviour::TELEPORTING:
+			break;
+	}
+}
+
+void AASDronePawn::ChasingBehaviour()
+{
+	float distanceToTarget;
+
+	if(IsPlayerInSight(distanceToTarget))
+	{
+		if(distanceToTarget < m_MinRangeToAttack)
+		{
+			//Move away from player
+			m_DroneToPlayerMult = -1;
+		}
+		else if(distanceToTarget > m_MaxRangeToAttack)
+		{
+			//Move towards player
+			m_DroneToPlayerMult = 1;
+		}
+		else ChangeBehaviour(EDroneBehaviour::SHOOTING);
+	}
+	else if(distanceToTarget < m_DistanceToTeleport)
+	{
+		ChangeBehaviour(EDroneBehaviour::TELEPORTING);
+	}
+}
+
+bool AASDronePawn::IsPlayerInSight(float& DistanceToTarget)
+{
+	FHitResult OutHit;
+	FVector StartLocation = GetActorLocation();
+	FVector EndLocation = m_Player->GetActorLocation();
+	ECollisionChannel CollisionChannel = static_cast<ECollisionChannel>(StaticEnum<ECollisionChannel>()->GetValueByName(TEXT("Drone_Hit")));
+	FCollisionQueryParams CollisionQueryParams = FCollisionQueryParams(SCENE_QUERY_STAT(SphereCast), true) ;
+	CollisionQueryParams.AddIgnoredActor(this);
+	
+	bool bHit = GetWorld()->SweepSingleByChannel(
+	OutHit,
+	StartLocation,
+	EndLocation,
+	FQuat::Identity,
+	CollisionChannel,
+	FCollisionShape::MakeSphere(m_SphereCastRadius),
+	CollisionQueryParams);
+	
+	if(bHit)
+	{
+		DistanceToTarget = OutHit.Distance;
+		if(OutHit.GetActor() == m_Player) return true;
+	}
+	return false;
+}
+
+void AASDronePawn::TeleportDrone()
+{
+	FCollisionQueryParams QueryParams;
+	QueryParams.bTraceComplex = false; 
+	QueryParams.bReturnPhysicalMaterial = false;
+	QueryParams.AddIgnoredActor(this);
+	QueryParams.AddIgnoredActor(m_Player);
+	ECollisionChannel CollisionChannel = static_cast<ECollisionChannel>(StaticEnum<ECollisionChannel>()->GetValueByName(TEXT("Drone_Hit")));
+
+	FCollisionShape CollisionShape;
+	CollisionShape.SetSphere(m_SphereCastRadius);
+	
+	bool tpPointIsClear = false;
+	int32 numberOfTeleportation = 0;
+	FVector TeleportationLocation = GetActorLocation();
+	while(tpPointIsClear == false)
+	{
+		numberOfTeleportation++;
+
+		FVector Direction = (m_Player->GetActorLocation() - GetActorLocation()).GetSafeNormal();
+		float TPDistance = (numberOfTeleportation * m_DistanceByTeleportationSphereCast);
+		FVector OverlapSphereLocation = GetActorLocation() +  Direction * TPDistance;
+
+		// Perform the overlap check
+		bool bHasOverlap = GetWorld()->OverlapBlockingTestByChannel(
+			OverlapSphereLocation,
+			FQuat::Identity,
+			CollisionChannel, 
+			CollisionShape,
+			QueryParams
+		);
+
+		if(!bHasOverlap)
+		{
+			TeleportationLocation = OverlapSphereLocation;
+			tpPointIsClear = true;
+		}
+		else if(numberOfTeleportation > m_NumberOfTPBeforeAbort)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("CANNOT TELEPORT THROUGH WALL"));
+			break;
+		}
+	}
+
+	SetActorLocation(TeleportationLocation);
+	ChangeBehaviour(EDroneBehaviour::CHASING);
+}
+
+void AASDronePawn::DroneMovement()
+{
+}
+
 void AASDronePawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	LookAtPlayer(DeltaTime);
+	OnBehaviourStateTick(DeltaTime);
+
+	m_AITickTimer -= DeltaTime;
+	if(m_AITickTimer <= 0)
+	{
+		m_AITickTimer = m_AITickRate;
+		OnBehaviourAIStateTick();
+	}
 }
 
-// Called to bind functionality to input
 void AASDronePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
--- Source/ArenaShooter/AI/Drone/ASDronePawn.h
+++ Source/ArenaShooter/AI/Drone/ASDronePawn.h
@@ -10,31 +10,101 @@
 class UASHealthComponent;
 class UASWeaponComponent;
 
+UENUM(BlueprintType) // Makes the enum available in Blueprints
+enum class EDroneBehaviour : uint8
+{
+	CHASING UMETA(DisplayName = "Chasing"),
+	SHOOTING UMETA(DisplayName = "Shooting"),
+	TELEPORTING UMETA(DisplayName = "Teleporting")
+};
+
 UCLASS()
 class ARENASHOOTER_API AASDronePawn : public AASPawn
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	AASDronePawn();
 
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour", DisplayName = "AI tick rate")
+	float m_AITickRate;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour", DisplayName = "Distance to trigger teleportation")
+	float m_DistanceToTeleport;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour", DisplayName = "Minimum distance to start shooting")
+	float m_MinRangeToAttack;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour", DisplayName = "Maximum distance to start shooting")
+	float m_MaxRangeToAttack;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour", DisplayName = "Radius of the detection sphere cast towards the player")
+	float m_SphereCastRadius;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour", DisplayName = "Time it takes to teleport through a wall")
+	float m_TeleportationDuration;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour", DisplayName = "Distance by sphere cast for teleportation")
+	int32 m_DistanceByTeleportationSphereCast;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour", DisplayName = "Number of teleportation raycast tests before abort")
+	int32 m_NumberOfTPBeforeAbort;
+	
+	UPROPERTY()
+	float m_AITickTimer;
+
+	UPROPERTY()
+	AActor* m_ObjectHit;
+
+	UPROPERTY()
+	float m_DroneToPlayerMult;
+
+	UPROPERTY()
+	float m_TeleportationTimer;
+
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	/** Player Reference */
 	UPROPERTY()
 	ACharacter* m_Player;
 
+	UPROPERTY()
+	EDroneBehaviour m_CurrentBehaviour;
+	
 	UFUNCTION()
 	virtual void LookAtPlayer(float DeltaTime);
+
+	UFUNCTION()
+	void ChangeBehaviour(EDroneBehaviour newBehaviour);
+
+	UFUNCTION()
+	void OnBehaviourStateEnter();
+
+	UFUNCTION()
+	void OnBehaviourStateExit();
+
+	UFUNCTION()
+	void OnBehaviourStateTick(float DeltaTime);
+
+	UFUNCTION()
+	void OnBehaviourAIStateTick();
+
+	UFUNCTION()
+	void ChasingBehaviour();
+	
+	UFUNCTION()
+	bool IsPlayerInSight(float& DistanceToTarget);
+
+	UFUNCTION()
+	void TeleportDrone();
+	
+	UFUNCTION()
+	void DroneMovement();
 	
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
