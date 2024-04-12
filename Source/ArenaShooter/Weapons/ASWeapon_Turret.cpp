e8ecaeac8554240596b44a4a87f9d2c8836e22be
Sneakysunset <andrelauzier@gmail.com>
2024-04-12 05:37:49 +0200

    Laser Damage

--- Source/ArenaShooter/Weapons/ASWeapon_Turret.cpp
+++ Source/ArenaShooter/Weapons/ASWeapon_Turret.cpp
@@ -1,13 +1,30 @@
 #include "ASWeapon_Turret.h"
 
+#include "ArenaShooter/AI/Drone/ASDronePawn.h"
+#include "ArenaShooter/Character/ASCharacter.h"
+#include "ArenaShooter/Components/ASHealthComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
 
+
+void AASWeapon_Turret::BeginPlay()
+{
+	Super::BeginPlay();
+	m_Character = Cast<AASCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
+}
+
+void AASWeapon_Turret::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	m_Deltatime = DeltaSeconds;
+}
 
 void AASWeapon_Turret::Fire(FVector fireOrigin, FVector fireDirection)
 {
 	Super::Fire(fireOrigin, fireDirection);
 
 
-	FHitResult HitResult;
+	/*FHitResult HitResult;
 	FCollisionQueryParams CollisionParams;
 	CollisionParams.bIgnoreTouches = false;
 	CollisionParams.AddIgnoredActor(GetOwner());
@@ -16,5 +33,38 @@ void AASWeapon_Turret::Fire(FVector fireOrigin, FVector fireDirection)
 	{
 		//if(OnFireEvent.IsBound())
 			OnFireEvent.Broadcast(HitResult.Location);
+	}*/
+
+
+	FHitResult OutHit;
+	TArray<AActor*> ActorsToIgnore {GetOwner()};
+	
+	ETraceTypeQuery TraceTypeQuery = UEngineTypes::ConvertToTraceType(ECC_GameTraceChannel3);
+	
+	bool bHit = UKismetSystemLibrary::SphereTraceSingle(
+	GetWorld(),
+	fireOrigin,
+	fireOrigin + fireDirection * 1000000,
+	m_LaserRadius,
+	TraceTypeQuery,
+	false,
+	ActorsToIgnore,
+	EDrawDebugTrace::ForOneFrame,
+	OutHit,
+	true,
+	FColor::Black
+	);
+
+	if(bHit)
+	{
+		OnFireEvent.Broadcast(OutHit.Location);
+		if(auto pawn = Cast<AASDronePawn>(OutHit.GetActor()))
+		{
+			UASHealthComponent::FindHealthComponent(pawn)->Damage(100, this);
+		}
+		else if(OutHit.GetActor() == m_Character)
+		{
+			UASHealthComponent::FindHealthComponent(m_Character)->Damage(m_Deltatime * m_DamageByBullet, this);
+		}
 	}
 }
